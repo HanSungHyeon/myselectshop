42be30ea69fae98ad092e397041d16e33d1417ba
HanSungHyeon <hsh5349@naver.com>
2024-07-30 14:50:41 +0900

    feat : 관심 상품 최저가 등록

--- src/main/java/com/sparta/myselectshop/product/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.sparta.myselectshop.product.controller;
 
+import com.sparta.myselectshop.product.dto.req.ProductMyPriceReqDto;
 import com.sparta.myselectshop.product.dto.req.ProductReqDto;
 import com.sparta.myselectshop.product.dto.res.ProductResDto;
 import com.sparta.myselectshop.product.entity.Product;
@@ -7,13 +8,12 @@ import com.sparta.myselectshop.product.mapper.ProductMapper;
 import com.sparta.myselectshop.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ProductController {
 	private final ProductService productService;
@@ -24,6 +24,21 @@ public class ProductController {
 	public ResponseEntity createProduct(@RequestBody ProductReqDto dto) {
 		Product product = productService.creatProduct(productMapper.toProduct(dto));
 		ProductResDto res = productMapper.toProductResDto(product);
+		return ResponseEntity.ok(res);
+	}
+
+	//조회
+	@GetMapping("/products")
+	public ResponseEntity findAllProductList() {
+		List<ProductResDto> res = productMapper.toProductResDtoList(productService.findAllProduct());
+
+		return ResponseEntity.ok(res);
+	}
+
+	@PutMapping("/products/{id}")
+	public ResponseEntity updateProduct(@PathVariable Long id, @RequestBody ProductMyPriceReqDto dto) throws IllegalAccessException {
+		ProductResDto res = productMapper.toProductResDto(productService.updateProduct(id, dto));
+
 		return ResponseEntity.ok(res);
 	}
 
